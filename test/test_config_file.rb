57cb6bddaf1081a1b09280e9976cb6b38af239a2
Ilya Sabanin <ilya.sabanin@gmail.com>
2011-07-28 17:20:50 +0800

    A bit more thorough test for ConfigFile.

--- test/test_config_file.rb
+++ test/test_config_file.rb
@@ -22,6 +22,7 @@ describe Mercurial::ConfigFile do
   
   it "should add settings" do
     @config.add_setting('hooks', 'incoming', 'hg update')
+    @config.contents.must_equal hgrc_with_incoming_hook_added
     @config.find_header('hooks').size.must_equal 3
     @config.find_setting('hooks', 'incoming').must_equal "incoming = hg update\n"
     @config.find_setting('hooks', 'commit').must_equal "commit =/Users/ilya/work/beanstalk/script/mercurial/commit.rb\n\n"
@@ -30,9 +31,43 @@ describe Mercurial::ConfigFile do
   
   it "should delete settings" do
     @config.delete_setting!('hooks', 'commit')
+    @config.contents.must_equal hgrc_with_commit_hook_deleted
     @config.find_header('hooks').size.must_equal 1
     @config.find_setting('hooks', 'commit').must_equal nil
     @config.find_setting('hooks', 'changegroup').must_equal "changegroup = /Users/ilya/work/beanstalk/script/mercurial/changegroup.rb\n"
+  end
+  
+private
+
+  def hgrc_with_incoming_hook_added
+    %q[[paths]
+default = http://selenic.com/hg
+
+[hooks]
+incoming = hg update
+changegroup = /Users/ilya/work/beanstalk/script/mercurial/changegroup.rb
+commit =/Users/ilya/work/beanstalk/script/mercurial/commit.rb
+
+[ui]
+username = Firstname Lastname <firstname.lastname@example.net>
+verbose = True
+
+# [revlog]
+# format=0 for revlog; format=1 for revlogng]
+  end
+  
+  def hgrc_with_commit_hook_deleted
+    %q[[paths]
+default = http://selenic.com/hg
+
+[hooks]
+changegroup = /Users/ilya/work/beanstalk/script/mercurial/changegroup.rb
+[ui]
+username = Firstname Lastname <firstname.lastname@example.net>
+verbose = True
+
+# [revlog]
+# format=0 for revlog; format=1 for revlogng]
   end
 
 end
