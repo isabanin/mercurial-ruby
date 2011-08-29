8d12bc2321abaf35aa3a9b336794000f770aba2c
Ilya Sabanin <ilya.sabanin@gmail.com>
2011-08-30 02:50:37 +0800

    Find exact matches of files only.

--- lib/mercurial-ruby/manifest.rb
+++ lib/mercurial-ruby/manifest.rb
@@ -16,8 +16,13 @@ module Mercurial
     
     def scan_for_path(path, revision=nil)
       revision ||= 'tip'
-      path = '' if path == '/'
-      hg(manifest_cmd(revision)).scan(/^(\w{40}) (\d{3}) (\*?) +(#{ path }.*)/)
+      path = path.gsub(/\/$/, '')
+      if path == '/' || path == ''
+        search_for = ".*"
+      else
+        search_for = "#{ path }$|#{ path }\/.*"
+      end
+      hg(manifest_cmd(revision)).scan(/^(\w{40}) (\d{3}) (\*?) +(#{ search_for })/)
     end
     
   private
