f600a0179ef75dd1f63f41f37860ec1370ba1e68
Ilya Sabanin <ilya.sabanin@gmail.com>
2012-03-03 20:41:27 -0500

    And even more docs.

--- lib/mercurial-ruby/factories/hook_factory.rb
+++ lib/mercurial-ruby/factories/hook_factory.rb
@@ -1,13 +1,23 @@
 module Mercurial
   
+  #
+  # This class is a handy way to manage hooks in your repository.
+  #
   class HookFactory
     
+    # Instance of {Mercurial::Repository Repository}.
     attr_reader :repository
     
     def initialize(repository)
       @repository = repository
     end
     
+    #
+    # Finds all repository hooks. Returns an array of {Mercurial::Hook Hook} instances.
+    #
+    # === Example:
+    #  repository.hooks.all
+    #
     def all
       [].tap do |returning|
         repository.config.find_header('hooks').each_pair do |name, value|
@@ -16,18 +26,36 @@ module Mercurial
       end
     end
     
+    #
+    # Finds a specific hook by it's name. Returns an instance of {Mercurial::Hook Hook}.
+    #
+    # === Example:
+    #  repository.hooks.by_name('changegroup')
+    #
     def by_name(name)
       all.find do |h|
         h.name == name.to_s
       end
     end
     
+    #
+    # Adds a new hook to the repository.
+    #
+    # === Example:
+    #  repository.hooks.add('changegroup', 'do_something')
+    #
     def add(name, value)
       build(name, value).tap do |hook|
         hook.save
       end
     end
     
+    #
+    # Removes a hook from the repository.
+    #
+    # === Example:
+    #  repository.hooks.remove('changegroup')
+    #
     def remove(name)
       if hook = by_name(name)
         hook.destroy!
